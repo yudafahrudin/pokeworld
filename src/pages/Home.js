6dafa444d33ee1f9eacdbf7e740c9aca3cf12301
yudafahrudin <yuda@vontis.id>
2022-04-16 23:42:39 +0700

    remove unused

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
 import { useLazyQuery } from "@apollo/client";
 import { css } from '@emotion/css'
 
@@ -16,7 +15,6 @@ const pageDetail = css`
 function Home() {
     const limit = 5;
     const offset = 0;
-    const navigate = useNavigate();
     const [pokemonList, setPokemonList] = useState([])
     const [isFetching, setIsFetching] = useState(false)
 
@@ -27,10 +25,6 @@ function Home() {
             setIsFetching(false);
         }
     })
-
-    const ImageMemo = ({ src }) => (React.useMemo(() => {
-        return <img src={src} />
-    }))
 
     useEffect(() => {
         window.addEventListener('scroll', handleScroll);
