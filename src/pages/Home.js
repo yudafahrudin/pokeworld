ee9890cb4c2f02df7124d83274d9cc3d0eca820a
yudafahrudin <yuda@vontis.id>
2022-04-16 23:42:17 +0700

    remove unused

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,8 +9,6 @@ import {
     GET_POKEMON_LIST
 } from '../graphql/queries'
 
-import { countMyPokemon } from '../helpers'
-
 const pageDetail = css`
     font-size:.85rem;
     padding:5px;
@@ -68,70 +66,6 @@ function Home() {
             }
         });
     }, [])
-
-    const handleRedirectPokemonDetail = (pokemonData) => {
-        const { name, image, dreamworld } = pokemonData;
-        navigate(`/pokemon-detail/${name}?image=${btoa(image)}&dreamworld=${btoa(dreamworld)}`)
-    }
-
-    // const PokemonCardList = ({ pokemon }) => {
-    //     return (
-    //         <li
-    //             className={css`
-    //             width:100%;
-    //             display:flex;
-    //             cursor: pointer;
-    //             border-radius:10px;
-    //             margin: 0 auto 10px auto;
-    //             box-shadow: rgba(9, 30, 66, 0.25) 0px 3px 4px -2px, rgba(9, 30, 66, 0.08) 0px 0px 0px 1px;
-    //             `}
-    //             onClick={() => handleRedirectPokemonDetail({
-    //                 name: pokemon.name,
-    //                 image: pokemon.image,
-    //                 dreamworld: pokemon.dreamworld
-    //             })}>
-    //             <div className={css`
-    //                 border-radius:10px;
-    //                 padding:5px;
-    //                 height:100px;
-    //                 width:100px;
-    //                 max-width:100px;
-    //                 `}>
-    //                 <ImageMemo src={pokemon.image} />
-    //                 {/* <img
-    //                     className={css`
-    //                     transition: all .5s;
-    //                     `}
-    //                     src={pokemon.image}
-    //                     width={"100%"}
-    //                     height={"auto"}
-    //                     alt={pokemon.name}
-    //                 /> */}
-    //             </div>
-    //             <div
-    //                 className={
-    //                     css`
-    //                     padding:10px
-    //                     `
-    //                 }
-    //             >
-    //                 <h3 className={
-    //                     css`
-    //                         margin-bottom: .5em;
-    //                         text-transform: capitalize;
-    //                         `
-    //                 }>
-    //                     {pokemon.name}
-    //                 </h3>
-    //                 <p className={css`
-    //                     font-size: .745rem;
-    //                     `}>
-    //                     you've owned: {countMyPokemon(pokemon.name)}
-    //                 </p>
-    //             </div>
-    //         </li>
-    //     )
-    // }
 
     return (
         <>
