9ff8df782c1b92b8bed01163bd407c7f281ca9c1
yudafahrudin <yuda@vontis.id>
2022-04-28 11:23:55 +0700

    add webpack minify

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,24 @@
+// Library
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 const TerserPlugin = require("terser-webpack-plugin");
 
+// Extention list
+const scriptExtensions = /\.(js|jsx)$/;
+const styleExtensions = /\.css$/i;
+const imageExtensions = /\.(bmp|gif|jpg|jpeg|png)$/i;
+
 module.exports = {
     entry: {
         index: "./src/index.js",
-        helpers: "./src/helpers/index.js",
-        components: "./src/components/index.js"
+        helper: "./src/helpers/index.js",
+        component: "./src/components/index.js"
     },
     output: {
         publicPath: '/',
-        filename: '[name]-[fullhash].chunk.js',
+        filename: '[name].[chunkhash].chunk.js',
         path: path.resolve(__dirname, 'dist'),
         clean: true,
     },
@@ -22,19 +28,19 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /\.(js|jsx)$/,
+                test: scriptExtensions,
                 exclude: /node_modules/,
                 use: {
                     loader: 'babel-loader'
                 }
             },
             {
-                test: /\.css$/i,
+                test: styleExtensions,
                 use: ["css-loader"],
                 use: [MiniCssExtractPlugin.loader, "css-loader"],
             },
             {
-                test: /\.(png|jpe?g|gif)$/i,
+                test: imageExtensions,
                 loader: 'file-loader',
                 options: {
                     esModule: false,
@@ -58,14 +64,20 @@ module.exports = {
             cacheGroups: {
                 vendor: {
                     test: /[\\/]node_modules[\\/]/,
-                    name: 'vendors',
+                    name: 'vendor',
                     chunks: 'all'
                 }
             }
         },
         minimizer: [
-            new CssMinimizerPlugin(),
-            new TerserPlugin()
+            new CssMinimizerPlugin(), // minify css
+            new TerserPlugin({
+                terserOptions: {
+                    format: {
+                        comments: false, // It will drop all the console.log statements from the final production build
+                    },
+                }
+            })
         ],
     },
     performance: {
